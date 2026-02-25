90a4fed0791d578c4e7979a77c7d348523778505
ShawnZhong <github@shawnzhong.com>
2026-02-25 17:34:36 +0000

    Update build.mk

--- third_party/build.mk
+++ third_party/build.mk
@@ -1,8 +1,10 @@
 MUSL_DIR := third_party/musl
 MUSL_PATCH := $(abspath third_party/musl.patch)
 
-.PHONY: patch
-patch:
+.PHONY: musl
+musl: $(BUILD_DIR)/libc.so
+
+$(BUILD_DIR)/libc.so: $(MUSL_PATCH) | $(BUILD_DIR)
 	@set -eu; \
 	if git -C $(MUSL_DIR) apply --check $(MUSL_PATCH) >/dev/null 2>&1; then \
 		git -C $(MUSL_DIR) apply $(MUSL_PATCH); \
@@ -12,9 +14,7 @@ patch:
 		echo "failed to apply $(MUSL_PATCH)" >&2; \
 		exit 1; \
 	fi
-
-.PHONY: musl
-musl: patch | $(BUILD_DIR)
+	cd $(MUSL_DIR) && ./configure
 	$(MAKE) -C $(MUSL_DIR)
 	cp $(MUSL_DIR)/lib/libc.so $(BUILD_DIR)/libc.musl-x86_64.so.1
 	ln -sfn libc.musl-x86_64.so.1 $(BUILD_DIR)/libc.so
